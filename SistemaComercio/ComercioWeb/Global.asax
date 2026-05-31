ec69204434d51822d3eaa0726ac4df1a9d0a78b5
Esteban Vladimir Reyes <estebanvladimirreyes@gmail.com>
2026-05-31 17:22:52 -0300

    Arquitectura en 3 capas y creación de clases modelo en capa Dominio

---
+++ SistemaComercio/ComercioWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComercioWeb.Global" Language="C#" %>
---
+++ SistemaComercio/Dominio/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string CUIT { get; set; }
+        public string RazonSocial { get; set; }
+    }
+}
