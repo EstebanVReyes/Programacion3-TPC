4718fe31be4180cdce1424d313bb39e1ea9a35ab
Esteban Vladimir Reyes <estebanvladimirreyes@gmail.com>
2026-05-31 18:11:30 -0300

    Completar modelo de dominio con clases de Venta, Compra y Detalles

---
+++ SistemaComercio/Dominio/Compra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public decimal Total { get; set; }
+        public List<DetalleCompra> Detalles { get; set; }
+    }
+}
---
+++ SistemaComercio/Dominio/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public string NumeroFactura { get; set; }
+        public DateTime Fecha { get; set; }
+        public Cliente Cliente { get; set; }
+        public decimal Total { get; set; }
+        public List<DetalleVenta> Detalles { get; set; }
+    }
+}
