ad8f3c3892a5aaf54f42b0380aca35ce228676b9
Esteban Reyes <estebanvladimirreyes@gmail.com>
2026-06-14 18:50:13 -0300

    creo clase cliente

---
+++ SistemaComercio/Dominio/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public List<Direccion> Direcciones { get; set; }
+                
+        public override string ToString()
+        {
+            return Nombre + " " + Apellido;
+        }
+    }
+}
